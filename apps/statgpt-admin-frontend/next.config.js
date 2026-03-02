303777312d5437799ac8354730cacce33bfa9657
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-03-02 12:01:24 +0100

    chore: update nx

--- apps/statgpt-admin-frontend/next.config.js
+++ apps/statgpt-admin-frontend/next.config.js
@@ -23,11 +23,7 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
+  nx: {},
   poweredByHeader: false,
   async headers() {
     return [
